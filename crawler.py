ff0356374fc9a0a4821146069f5803083a5e5c7b
junhong91 <junhong199@gmail.com>
2022-06-25 21:05:23 +0900

    Implements web ui crawler

---
+++ crawler.py
@@ -0,0 +1,45 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+driver = webdriver.Chrome(os.environ['CHROME_DRIVER_PATH'])
+real_estate_agency_search_url = os.environ['REAL_ESTATE_AGENCY_SEARCH_URL']
+
+# 시/도 선택 search box xpath
+sido_search_box_xpath = '//*[@id="shSido"]'
+# 시/군/구 선택 search box xpath
+sigungu_search_box_xpath = '//*[@id="shSigungu"]'
+# 등록번호
+registration_number_input_xpath = '//*[@id="shWord1"]'
+# 검색 버튼
+search_button_xpath = '//*[@id="icon_btn_write"]'
+
+
+def main():
+    driver.get(real_estate_agency_search_url)
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable(
+            (By.XPATH, sido_search_box_xpath))).send_keys("서울특별시")
+
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable(
+            (By.XPATH, sigungu_search_box_xpath))).send_keys("강동구")
+
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable(
+            (By.XPATH,
+             registration_number_input_xpath))).send_keys("11740-2016-00185")
+
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, search_button_xpath))).click()
+
+
+if __name__ == "__main__":
+    main()
