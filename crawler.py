d2481aeae5e92a74dfb48fe6e37c4bace6e288d9
junhong91 <junhong199@gmail.com>
2022-06-25 21:46:03 +0900

    Parse data using beautifulSoup

--- crawler.py
+++ crawler.py
@@ -2,7 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-import time
+
+from bs4 import BeautifulSoup
 
 import os
 from dotenv import load_dotenv
@@ -20,6 +21,8 @@ sigungu_search_box_xpath = '//*[@id="shSigungu"]'
 registration_number_input_xpath = '//*[@id="shWord1"]'
 # 검색 버튼
 search_button_xpath = '//*[@id="icon_btn_write"]'
+# 검색 결과 하이퍼 링크 버튼
+search_result_href_xpath = '//*[@id="searchVO"]/div[2]/table/tbody/tr/td[3]/a'
 
 
 def main():
@@ -39,6 +42,17 @@ def main():
 
     WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.XPATH, search_button_xpath))).click()
+
+    WebDriverWait(driver, 120).until(
+        EC.element_to_be_clickable(
+            (By.XPATH, search_result_href_xpath))).click()
+
+    # HTML 구문 분석 & 데이터 추출
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    contents = soup.findAll("td")
+    data = [data.text.strip() for data in contents]
+    print(data)
 
 
 if __name__ == "__main__":
